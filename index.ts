016368cf1d01877be80003aedb3d2ca3029aeb60
Andrii Gubanov <andrey.a.gubanov@gmail.com>
2024-02-16 20:34:48 +0100

    fix: Init Worker

--- index.ts
+++ index.ts
@@ -1,1 +1,8 @@
 export * from './.vovk';
+import { WorkerService, WorkerYieldService } from './.vovk';
+
+if (typeof Worker !== 'undefined') {
+  WorkerService.use(new Worker(new URL('./src/modules/worker/WorkerService.ts', import.meta.url)));
+  WorkerYieldService.use(new Worker(new URL('./src/modules/worker-yield/WorkerYieldService.ts', import.meta.url)));
+}
+export { WorkerService };
