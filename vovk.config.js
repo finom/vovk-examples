ede476b8b0ab2a625c327b0fc0f4e2d9d5ee11fe
Andrii Gubanov <andrey.a.gubanov@gmail.com>
2024-02-16 09:29:45 +0100

    fix: Minor

--- vovk.config.js
+++ vovk.config.js
@@ -1,8 +1,7 @@
-const vc = process.env.NEXT_PUBLIC_VERCEL_URL;
-
 /** @type {import('vovk').VovkRc} */
 const config = {
-  prefix: vc ? `https://${vc}/api` : `http://localhost:${process.env.PORT}/api`,
+  // you can use NEXT_PUBLIC_VERCEL_URL env var for preview deployments
+  prefix: process.env.VERCEL_ENV ? `https://vovk-examples.vercel.app/api` : `http://localhost:${process.env.PORT}/api`,
 };
 
 module.exports = config;
