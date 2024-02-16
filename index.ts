1bae8d9c4c4cadf041362b79c63801a6fc655e9b
Andrii Gubanov <andrey.a.gubanov@gmail.com>
2024-02-16 22:34:56 +0100

    fix: Minor

--- index.ts
+++ index.ts
@@ -1,8 +1,8 @@
 export * from './.vovk';
-import { WorkerService, WorkerYieldService } from './.vovk';
+import { WorkerService } from './.vovk';
 import WorkerServiceWorker from 'worker-loader!./src/modules/worker/WorkerService';
-
+console.log(WorkerService, WorkerServiceWorker);
 // @ts-ignore
 WorkerService.use(new (WorkerServiceWorker as unknown as typeof Worker)());
 
-export { WorkerService, WorkerYieldService };
+export { WorkerService };
