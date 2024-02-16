3c4a08a6c44f184ef69d32f3880c7dc1fca41544
Andrii Gubanov <andrey.a.gubanov@gmail.com>
2024-02-16 23:32:40 +0100

    fix: Minor

--- index.ts
+++ index.ts
@@ -1,7 +1,9 @@
-import { WorkerService, StreamController } from './.vovk';
+import { WorkerService, WorkerYieldService, StreamController } from './.vovk';
 import WorkerServiceWorker from 'worker-loader?inline=fallback!./src/modules/worker/WorkerService';
+import WorkerYieldServiceWorker from 'worker-loader?inline=fallback!./src/modules/yield-worker/WorkerYieldService';
 
 if (typeof Worker !== 'undefined') {
   WorkerService.use(new (WorkerServiceWorker as unknown as typeof Worker)(''));
+  WorkerYieldService.use(new (WorkerYieldServiceWorker as unknown as typeof Worker)(''));
 }
 export { WorkerService, StreamController };
