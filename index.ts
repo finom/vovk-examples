3dd6a728bd063c15530082382646d2f92dc69296
Andrii Gubanov <andrey.a.gubanov@gmail.com>
2024-02-16 20:49:22 +0100

    fix: Remove workers

--- index.ts
+++ index.ts
@@ -1,8 +1,1 @@
 export * from './.vovk';
-import { WorkerService, WorkerYieldService } from './.vovk';
-
-if (typeof Worker !== 'undefined') {
-  WorkerService.use(new Worker(new URL('./src/modules/worker/WorkerService.ts', import.meta.url)));
-  WorkerYieldService.use(new Worker(new URL('./src/modules/worker-yield/WorkerYieldService.ts', import.meta.url)));
-}
-export { WorkerService };
