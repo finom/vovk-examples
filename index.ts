bc1bba0322294743191f7cc4b86defa45fdb7e86
Andrii Gubanov <andrey.a.gubanov@gmail.com>
2024-02-17 09:29:07 +0100

    fix: Minor

--- index.ts
+++ index.ts
@@ -1,9 +1,15 @@
 import { WorkerService, WorkerYieldService, StreamController } from './.vovk';
-import WorkerServiceWorker from 'worker-loader?inline=fallback!./src/modules/worker/WorkerService';
+/* import WorkerServiceWorker from 'worker-loader?inline=fallback!./src/modules/worker/WorkerService';
 import WorkerYieldServiceWorker from 'worker-loader?inline=fallback!./src/modules/worker-yield/WorkerYieldService';
 
 if (typeof Worker !== 'undefined') {
   WorkerService.use(new (WorkerServiceWorker as unknown as typeof Worker)(''));
   WorkerYieldService.use(new (WorkerYieldServiceWorker as unknown as typeof Worker)(''));
+} */
+
+if (typeof Worker !== 'undefined') {
+  WorkerService.use(new Worker(new URL('./src/modules/worker/WorkerService.ts?inline', import.meta.url)));
+  // WorkerYieldService.use(new Worker(new URL('./relative/path/to/my/worker.js?inline', import.meta.url)));
 }
+
 export { WorkerService, StreamController };
--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,10 @@ module.exports = {
         },
       }, */
       {
+        resourceQuery: /inline/,
+        type: 'asset/inline',
+      },
+      {
         test: /\.ts$/,
         use: [
           {
