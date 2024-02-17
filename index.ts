a47b3ef0621e48a08e77fef834ab904d48fdd7c6
Andrii Gubanov <andrey.a.gubanov@gmail.com>
2024-02-17 15:56:20 +0100

    fix: Revert

--- index.ts
+++ index.ts
@@ -1,6 +1,1 @@
 export * from './.vovk';
-import { WorkerService } from './.vovk';
-
-WorkerService.use(new Worker(new URL('vovk-examples/dist/WorkerService.js', import.meta.url)));
-
-export { WorkerService };
