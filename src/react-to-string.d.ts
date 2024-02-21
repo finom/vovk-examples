cc6606597b6cd299b331a14e6c40f595d782dae3
Andrii Gubanov <andrey.a.gubanov@gmail.com>
2024-02-21 15:38:34 +0100

    fix: Add react-to-string.d.ts

---
+++ src/react-to-string.d.ts
@@ -0,0 +1,7 @@
+declare module 'react-to-string' {
+  declare const reactToString: (
+    element: React.ReactElement | React.ReactNode | string | Array<string | React.ReactElement>
+  ) => string;
+
+  export default reactToString;
+}
