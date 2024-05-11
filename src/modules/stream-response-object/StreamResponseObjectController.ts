ab3f244a445c318b64fca9630fbb08622ac4a753
Andrii Gubanov <andrey.a.gubanov@gmail.com>
2024-05-11 16:41:26 +0200

    fix: Cors headers for StreamResponseObjectController

--- src/modules/stream-response-object/StreamResponseObjectController.ts
+++ src/modules/stream-response-object/StreamResponseObjectController.ts
@@ -3,9 +3,17 @@ import StreamService, { type Token } from './StreamService';
 
 @prefix('stream-with-object')
 export default class StreamResponseObjectController {
-  @get('tokens', { cors: true })
+  @get('tokens')
   static async streamTokens() {
-    const response = new StreamResponse<Token>();
+    // need to define CORS headers manually when returning Response instance
+    const response = new StreamResponse<Token>({
+      headers: {
+        ...StreamResponse.defaultHeaders,
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS, HEAD',
+        'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+      },
+    });
 
     void StreamService.streamTokens(response);
 
