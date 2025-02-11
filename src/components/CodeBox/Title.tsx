5b083bc7732a2e8efb607419caa5b36b7cc7f721
Andrey Gubanov <andrii@gubanov.eu>
2025-02-11 11:32:37 +0100

    chore: Renew Tailwind (buggy)

--- src/components/CodeBox/Title.tsx
+++ src/components/CodeBox/Title.tsx
@@ -20,7 +20,7 @@ const Title = ({ href, children }: Props) => {
   );
 
   const className = `
-      bg-gradient-to-br from-[#f2f2fd] to-[#f7f7ff]
+      bg-linear-to-br from-[#f2f2fd] to-[#f7f7ff]
       dark:bg-gray-950 dark:text-white dark:[background-image:none]
       p-[3px] flex items-center justify-between w-full py-1`;
   return href ? (
