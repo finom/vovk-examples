ca2b709c3e324d3a4c58dcecd392a3b3547495cb
Andrey Gubanov <andrii@gubanov.eu>
2025-07-11 17:01:09 +0200

    minor

--- vovk.config.mjs
+++ vovk.config.mjs
@@ -11,19 +11,18 @@ const config = {
   },
   bundle: {
     readme: {
-      banner: `
-    <p align="center"> 
-      <picture>
-        <source width="300" media="(prefers-color-scheme: dark)" srcset="https://vovk.dev/vovk-logo-white.svg">
-        <source width="300" media="(prefers-color-scheme: light)" srcset="https://vovk.dev/vovk-logo.svg">
-        <img width="300" alt="vovk" src="https://vovk.dev/vovk-logo.svg">
-      </picture><br>
-      <strong>RESTful + RPC = ♥️</strong>
-    </p>
+      banner: `<p align="center"> 
+  <picture>
+    <source width="300" media="(prefers-color-scheme: dark)" srcset="https://vovk.dev/vovk-logo-white.svg">
+    <source width="300" media="(prefers-color-scheme: light)" srcset="https://vovk.dev/vovk-logo.svg">
+    <img width="300" alt="vovk" src="https://vovk.dev/vovk-logo.svg">
+  </picture><br>
+  <strong>RESTful + RPC = ♥️</strong>
+</p>
 
-    <p align="center">
-      Back-end meta-framework for <a href="https://nextjs.org/docs/app">Next.js</a>
-    </p>`,
+<p align="center">
+  Back-end meta-framework for <a href="https://nextjs.org/docs/app">Next.js</a>
+</p>`,
     },
   },
   emitConfig: true,
