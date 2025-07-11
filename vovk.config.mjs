bd4fa8299d1b447f758ee4ed6f5d1eafd18a7874
Andrey Gubanov <andrii@gubanov.eu>
2025-07-11 16:59:34 +0200

    minor

--- vovk.config.mjs
+++ vovk.config.mjs
@@ -1,3 +1,4 @@
+// @ts-check
 /** @type {import('vovk').VovkConfig} */
 const config = {
   // you can use NEXT_PUBLIC_VERCEL_URL env var for preview deployments on Vercel
@@ -7,6 +8,23 @@ const config = {
   imports: {
     createRPC: 'vovk-react-query',
     validateOnClient: 'vovk-ajv',
+  },
+  bundle: {
+    readme: {
+      banner: `
+    <p align="center"> 
+      <picture>
+        <source width="300" media="(prefers-color-scheme: dark)" srcset="https://vovk.dev/vovk-logo-white.svg">
+        <source width="300" media="(prefers-color-scheme: light)" srcset="https://vovk.dev/vovk-logo.svg">
+        <img width="300" alt="vovk" src="https://vovk.dev/vovk-logo.svg">
+      </picture><br>
+      <strong>RESTful + RPC = ♥️</strong>
+    </p>
+
+    <p align="center">
+      Back-end meta-framework for <a href="https://nextjs.org/docs/app">Next.js</a>
+    </p>`,
+    },
   },
   emitConfig: true,
   logLevel: 'debug',
