9a94c072b3809e70330f4746d1de81827e05fd7e
Andrey Gubanov <andrii@gubanov.eu>
2025-07-26 17:30:42 +0200

    minor

--- vovk.config.mjs
+++ vovk.config.mjs
@@ -2,7 +2,7 @@
 /** @type {import('vovk').VovkConfig} */
 const config = {
   // you can use NEXT_PUBLIC_VERCEL_URL env var for preview deployments on Vercel
-  origin: process.env.VERCEL_ENV
+  origin: process.env.NODE_ENV === 'production'
     ? `https://vovk-examples.vercel.app`
     : 'http://localhost:' + (process.env.PORT ?? 3000),
   imports: {
