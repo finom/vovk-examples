496f574a0e37ce2bafe7ab4fd64d796ad38a4d9b
Andrey Gubanov <andrii@gubanov.eu>
2025-08-28 20:11:20 +0200

    fix: Renew deps

--- vovk.config.mjs
+++ vovk.config.mjs
@@ -2,24 +2,27 @@
 /** @type {import('vovk').VovkConfig} */
 const config = {
   // you can use NEXT_PUBLIC_VERCEL_URL env var for preview deployments on Vercel
-  origin:
-    process.env.NODE_ENV === 'production'
-      ? `https://vovk-examples.vercel.app`
-      : 'http://localhost:' + (process.env.PORT ?? 3000),
-  imports: {
-    validateOnClient: 'vovk-ajv',
+  generatorConfig: {
+    origin:
+      process.env.NODE_ENV === 'production'
+        ? `https://vovk-examples.vercel.app`
+        : 'http://localhost:' + (process.env.PORT ?? 3000),
+    imports: {
+      validateOnClient: 'vovk-ajv',
+    },
   },
   bundle: {
     tsdownBuildOptions: {
       tsconfig: './tsconfig.bundle.json',
     },
-    reExports: {
-      'default as ProgressiveExample': './src/app/progressive/ProgressiveExample',
-      'default as JSONLinesExample': './src/app/jsonlines/JSONLinesExample',
-      'default as getGithubFiles': '@/lib/getGithubFiles',
-    },
-    readme: {
-      banner: `<p align="center"> 
+    generatorConfig: {
+      reExports: {
+        'default as ProgressiveExample': './src/app/progressive/ProgressiveExample',
+        'default as JSONLinesExample': './src/app/jsonlines/JSONLinesExample',
+        'default as getGithubFiles': '@/lib/getGithubFiles',
+      },
+      readme: {
+        banner: `<p align="center"> 
   <picture>
     <source width="300" media="(prefers-color-scheme: dark)" srcset="https://vovk.dev/vovk-logo-white.svg">
     <source width="300" media="(prefers-color-scheme: light)" srcset="https://vovk.dev/vovk-logo.svg">
@@ -31,10 +34,11 @@ const config = {
 <p align="center">
   Back-end meta-framework for <a href="https://nextjs.org/docs/app">Next.js</a>
 </p>`,
-    },
-    package: {
-      dependencies: {
-        'react-loading-skeleton': '^3.5.0',
+      },
+      package: {
+        dependencies: {
+          'react-loading-skeleton': '^3.5.0',
+        },
       },
     },
   },
